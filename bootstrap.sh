6ce51fd6d881afc499fac5b85d8d8adf45464a36
Dragomir Yordanov <bad.stupid.monkey@gmail.com>
2018-09-20 02:24:04 +0200

    Fix php service restart

--- bootstrap.sh
+++ bootstrap.sh
@@ -200,6 +200,6 @@ sed -ie 's/#force_color_prompt=/force_color_prompt=/g' ~/.bashrc
 # ------------------------------------------
 # Restart services
 # ------------------------------------------
-sudo service php restart
+sudo service php7.2-fpm restart
 sudo service nginx restart
 sudo service mysql restart
